14a93d78a1b7f6dcd7fc4d1aadab62eecddb42a1
paulorsp2021 <paulorsp2021@gmail.com>
2025-02-19 12:02:17 -0300

    adicionadas classes de security

---
+++ src/main/java/com/javapaulo/usuario/infrastructure/repository/UsuarioRepository.java
@@ -0,0 +1,20 @@
+package com.javapaulo.usuario.infrastructure.repository;
+
+import com.javapaulo.usuario.infrastructure.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    boolean existsByEmail(String email);
+
+    Optional<Usuario> findByEmail(String email);
+
+    @Transactional
+    void deleteByEmail(String email);
+
+}
