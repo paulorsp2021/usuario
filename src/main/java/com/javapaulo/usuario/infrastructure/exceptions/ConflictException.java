d1f220a5992da5b9bceb686320402a16a41ff059
paulorsp2021 <paulorsp2021@gmail.com>
2025-02-19 16:46:04 -0300

    salva usuario

--- src/main/java/com/javapaulo/usuario/infrastructure/exceptions/ConflictException.java
+++ src/main/java/com/javapaulo/usuario/infrastructure/exceptions/ConflictException.java
@@ -5,7 +5,6 @@ public class ConflictException extends RuntimeException{
     public ConflictException(String mensagem){
         super(mensagem);
     }
-
     public ConflictException(String mensagem, Throwable throwable){
         super(mensagem);
     }
